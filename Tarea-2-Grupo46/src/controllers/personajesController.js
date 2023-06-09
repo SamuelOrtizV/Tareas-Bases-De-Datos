f6b24bd3a2b22671dfc51dfc55231cc4cf6da5cd
SamuelOrtizV <samuel.ortizv@usm.cl>
2023-06-08 20:19:12 -0400

    Se agregan varios controladores y se actualizan excepciones en personajesControllers
    Se agregan los controladores CRUD para:
    
    -defensas
    -diplomacias
    -personaje_habita_reino
    -reino_tiene_defensas
    
    Y se agregaros casos de excepciones para los personajes

--- Tarea-2-Grupo46/src/controllers/personajesController.js
+++ Tarea-2-Grupo46/src/controllers/personajesController.js
@@ -1,22 +1,42 @@
 import prisma from '../prismaClient.js'
 
 const getPersonajes = async (req , res) => {
-    const personajes = await prisma.personajes.findMany()
-    res.json(personajes)
+    try {
+        const personajes = await prisma.personajes.findMany();
+        res.json(personajes);
+      } catch (error) {
+        res.status(500).json({ error: 'Error al obtener los personajes' }); // 500: Internal Server Error
+      }
 }
 
 
-const getPersonajeById = async (req, res) => {
+/* const getPersonajeById = async (req, res) => {
     const { id } = req.params
     const personaje = await prisma.personajes.findUnique({
-        where: {
-            id: Number(id)
-        }
+        where: {id: Number(id)}
     })
     res.json(personaje)
-}
+} */
 
-const addPersonaje = async (req, res) => {
+// Obtener un personaje por su ID
+
+const getPersonajeById = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const personaje = await prisma.personajes.findUnique({
+        where: { id: parseInt(id) },
+      });
+      if (personaje) {
+        res.json(personaje);
+      } else {
+        res.status(404).json({ message: 'Personaje no encontrado' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener el personaje' });
+    }
+  };
+
+/* const addPersonaje = async (req, res) => {
     const { nombre, fuerza, fecha_nacimiento, objeto } = req.body
     const personaje = await prisma.personajes.create({
         data: {
@@ -27,9 +47,27 @@ const addPersonaje = async (req, res) => {
         }
     })
     res.json(personaje)
-}
+} */
 
-const updatePersonaje = async (req, res) => {
+// Agregar un nuevo personaje
+const addPersonaje = async (req, res) => {
+    const { nombre, fuerza, fecha_nacimiento, objeto } = req.body;
+    try {
+      const nuevoPersonaje = await prisma.personajes.create({
+        data: {
+          nombre,
+          fuerza,
+          fecha_nacimiento,
+          objeto,
+        },
+      });
+      res.status(201).json(nuevoPersonaje);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al agregar el personaje' });
+    }
+  };
+
+/* const updatePersonaje = async (req, res) => {
     const { id } = req.params
     const datosActualizados = req.body;
     const personaje = await prisma.personajes.update({
@@ -39,9 +77,29 @@ const updatePersonaje = async (req, res) => {
         data: datosActualizados
     })
     res.json(personaje)
-}
+} */
 
-const deletePersonaje = async (req, res) => {
+// Actualizar un personaje
+const updatePersonaje = async (req, res) => {
+    const { id } = req.params;
+    const { nombre, fuerza, fecha_nacimiento, objeto } = req.body;
+    try {
+      const personajeActualizado = await prisma.personajes.update({
+        where: { id: parseInt(id) },
+        data: {
+          nombre,
+          fuerza,
+          fecha_nacimiento,
+          objeto,
+        },
+      });
+      res.json(personajeActualizado);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al actualizar el personaje' });
+    }
+  };
+
+/* const deletePersonaje = async (req, res) => {
     const { id } = req.params
     const personaje = await prisma.personajes.delete({
         where: {
@@ -49,7 +107,20 @@ const deletePersonaje = async (req, res) => {
         }
     })
     res.json(personaje)
-}
+} */
+
+// Eliminar un personaje
+const deletePersonaje = async (req, res) => {
+    const { id } = req.params;
+    try {
+      await prisma.personajes.delete({
+        where: { id: parseInt(id) },
+      });
+      res.json({ message: 'Personaje eliminado correctamente' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error al eliminar el personaje' });
+    }
+  };
 
 
 const personajesController = {
